7088fe6dd12695943a9505a618d6824d971cedb5
Joe Stephen <joestephenk10@gmail.com>
2024-06-20 15:53:58 +0530

    final commit

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,4 +1,5 @@
 let sound;
+
 document.addEventListener("keydown", logKey);
 function logKey(e) {
   const key = e.code;
@@ -7,32 +8,47 @@ function logKey(e) {
     case "KeyW":
       sound = new Audio("./sounds/crash.mp3");
       sound.play();
+      animateKey("w");
       break;
     case "KeyA":
       sound = new Audio("./sounds/kick-bass.mp3");
       sound.play();
+      animateKey("a");
       break;
     case "KeyS":
       sound = new Audio("./sounds/snare.mp3");
       sound.play();
+      animateKey("s");
       break;
     case "KeyD":
       sound = new Audio("./sounds/tom-1.mp3");
       sound.play();
+      animateKey("d");
       break;
     case "KeyJ":
       sound = new Audio("./sounds/tom-2.mp3");
       sound.play();
+      animateKey("j");
       break;
     case "KeyK":
       sound = new Audio("./sounds/tom-3.mp3");
       sound.play();
+      animateKey("k");
       break;
     case "KeyL":
       sound = new Audio("./sounds/tom-4.mp3");
       sound.play();
+      animateKey("l");
       break;
     default:
       console.log("No key matched!");
   }
+}
+
+function animateKey(key) {
+  const activeKey = document.querySelector("." + key);
+  activeKey.classList.add("pressed");
+  setTimeout(() => {
+    activeKey.classList.remove("pressed");
+  }, 100);
 }
